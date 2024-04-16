6d185f9d3102e39db2f732cf8d4813d2e2f8822f
Viacheslav Korbut <minowaman777@gmail.com>
2024-04-16 13:44:51 +0300

    added description to main class

--- hw_01_statistic_json_parser/src/main/java/org/profitsoft/AppStatisticJSONParser.java
+++ hw_01_statistic_json_parser/src/main/java/org/profitsoft/AppStatisticJSONParser.java
@@ -14,10 +14,24 @@ import java.util.Map;
  * Date: 15.04.2024
  */
 public class AppStatisticJSONParser {
-
+    /**
+     * The path to the folder containing JSON files with book data.
+     */
     private static final String JSON_FILE_PATH = "hw_01_statistic_json_parser/src/main/resources/json_files";
+    /**
+     * The attribute based on which statistics will be calculated.
+     */
     private static final String ATTRIBUTE = "year_published";
 
+    /**
+     * The main method of the application. It performs the following tasks:
+     * 1. Parses JSON files containing book data from the specified folder.
+     * 2. Calculates the time taken to parse the JSON files.
+     * 3. Calculates statistics based on the specified attribute for the parsed book data.
+     * 4. Writes the calculated statistics to an XML file.
+     *
+     * @param args The command-line arguments (not used).
+     */
     public static void main(String[] args) {
         //Fixed start current time
         long startTime = System.currentTimeMillis();
