6491d6c624235ff9f9535da7a2e166a37d40818f
Viacheslav <minowaman777@gmail.com>
2024-06-12 18:33:24 +0300

    update EmailSender

---
+++ hw_05_1_email_sender/src/main/java/org/profitsoft/EmailSender.java
@@ -0,0 +1,18 @@
+package org.profitsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Viacheslav Korbut
+ * Date: 11.06.2024
+ */
+
+@SpringBootApplication
+public class EmailSender {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSender.class, args);
+    }
+
+}
