9206721c233cdf9447c9b3cf8ef88b989a68c0fc
Viacheslav <minowaman777@gmail.com>
2024-06-26 20:40:59 +0300

    added gateway

---
+++ hw_05_2_gateway_oauth/src/main/java/org/profitsoft/hw_05_2_gateway_oauth/Hw052GatewayOauthApplication.java
@@ -0,0 +1,13 @@
+package org.profitsoft.hw_05_2_gateway_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw052GatewayOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw052GatewayOauthApplication.class, args);
+    }
+
+}
